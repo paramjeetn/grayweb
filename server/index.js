9dcb32c65031c85515c6d650589a9840d3ea5875
Paramjeet <paramjeetnpradhan@gmail.com>
2024-01-08 07:41:52 +0530

    vercel commit

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(cors(
     {
-        origin: "*",
+        origin: "https://grayweb.vercel.app",
         credentials: true
       }
 ));
